e1c484741a9aa78ce5eb2335758e75130ec7e330
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-11-24 04:17:51 +0800

    remove user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_23_200545) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_23_201533) do
   create_table "posts", force: :cascade do |t|
     t.string "superhero"
     t.string "secret_identity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
