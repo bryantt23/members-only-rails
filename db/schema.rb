357466326b28ee4490a746fa6b6a4e2d76e6a3e2
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-11-24 07:57:31 +0800

    add user id to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_23_203231) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_23_235651) do
   create_table "posts", force: :cascade do |t|
     t.string "superhero"
     t.string "secret_identity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
